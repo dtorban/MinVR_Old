85eeb8117da116d484d2dd4f08c73d89b97daf6a
Dan Orban <dtorban@umn.edu>
2016-07-15 09:50:00 -0500

    Compressed image to be sent over web sockets.

--- plugins/OpenGL/src/VRReadPixelsNode.cpp
+++ plugins/OpenGL/src/VRReadPixelsNode.cpp
@@ -10,7 +10,23 @@
 #include "VROpenGLHeaders.h"
 #include "config/base64/base64.h"
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <cstddef>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <fstream>
+#include "config/base64/base64.h"
+
+#include "jpeglib.h"
+
+
+using namespace std;
+
 namespace MinVR {
+
+int compressImage(uint8_t * in_buffer, JOCTET * out_buffer, int width, int height);
 
 VRReadPixelsNode::VRReadPixelsNode(const std::string& name) : VRDisplayNode(name) {
 }
@@ -26,14 +42,20 @@ void VRReadPixelsNode::render(VRDataIndex* renderState,
 
 	VRDisplayNode::render(renderState, renderHandler);
 
-	GLubyte* pixels = (GLubyte*) malloc(width * height * sizeof(GLubyte) * 4);
+	GLubyte* pixels = (GLubyte*) malloc(width * height * sizeof(GLubyte) * 3);
 	glFinish();
 	glReadBuffer(GL_BACK);
-	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 	unsigned char* p = (reinterpret_cast<unsigned char *>(pixels));
 
-	std::string pixels64 = base64_encode(p, width*height*4);
+	JOCTET * out_buffer   = new JOCTET[width * height *3];
+	int size = compressImage(p, out_buffer, width, height);
+
+	std::string pixels64 = base64_encode(out_buffer, size);
+	//std::string pixels64 = base64_encode(p, width*height*3);
+
 	free(pixels);
+	delete[] out_buffer;
 
 	renderState->addData("pixels", pixels64);
 
@@ -46,5 +68,90 @@ VRDisplayNode* VRReadPixelsNode::create(VRMainInterface* vrMain,
 		VRDataIndex* config, const std::string& nameSpace) {
 	return new VRReadPixelsNode(nameSpace);
 }
+
+
+
+
+/* setup the buffer but we did that in the main function */
+void init_buffer(jpeg_compress_struct* cinfo) {}
+
+/* what to do when the buffer is full; this should almost never
+ * happen since we allocated our buffer to be big to start with
+ */
+boolean empty_buffer(jpeg_compress_struct* cinfo) {
+	return TRUE;
+}
+
+/* finalize the buffer and do any cleanup stuff */
+void term_buffer(jpeg_compress_struct* cinfo) {}
+
+int compressImage(uint8_t * in_buffer, JOCTET * out_buffer, int width, int height) {
+	/* load the image and note I assume this is a RGB 24 bit image
+	 * you should do more checking/conversion if possible
+	 */
+	//FILE        *outfile  = fopen("/tmp/test.jpeg", "wb");
+
+	//if (!outfile) throw runtime_error("Problem opening output file");
+
+	struct jpeg_compress_struct cinfo;
+	struct jpeg_error_mgr       jerr;
+	struct jpeg_destination_mgr dmgr;
+
+	/* create our in-memory output buffer to hold the jpeg */
+	//JOCTET * out_buffer   = new JOCTET[width * height *3];
+
+	/* here is the magic */
+	dmgr.init_destination    = init_buffer;
+	dmgr.empty_output_buffer = empty_buffer;
+	dmgr.term_destination    = term_buffer;
+	dmgr.next_output_byte    = out_buffer;
+	dmgr.free_in_buffer      = width * height *3;
+
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_compress(&cinfo);
+
+	/* make sure we tell it about our manager */
+	cinfo.dest = &dmgr;
+
+	cinfo.image_width      = width;
+	cinfo.image_height     = height;
+	cinfo.input_components = 3;
+	cinfo.in_color_space   = JCS_RGB;
+
+	jpeg_set_defaults(&cinfo);
+	jpeg_set_quality (&cinfo, 75, true);
+	jpeg_start_compress(&cinfo, true);
+
+	JSAMPROW row_pointer;
+	/*uint8_t *buffer    = new uint8_t[width * height*3];
+
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			buffer[3*(x*height+y) + 0] = 255*(1.0f*x/512.0);
+			buffer[3*(x*height+y) + 1] = 0;
+			buffer[3*(x*height+y) + 2] = 0;
+		}
+	}*/
+
+	/* main code to write jpeg data */
+	while (cinfo.next_scanline < cinfo.image_height) {
+		row_pointer = (JSAMPROW) &in_buffer[cinfo.next_scanline * width*3];
+		jpeg_write_scanlines(&cinfo, &row_pointer, 1);
+	}
+	jpeg_finish_compress(&cinfo);
+
+	/* write the buffer to disk so you can see the image */
+	//fwrite(out_buffer, cinfo.dest->next_output_byte - out_buffer,1 ,outfile);
+	std::cout << (cinfo.dest->next_output_byte - out_buffer) << std::endl;
+
+	//std::string ebuffer = base64_encode(out_buffer, cinfo.dest->next_output_byte - out_buffer);
+	//std::cout << ebuffer << std::endl;
+
+	//delete[] buffer;
+
+	return  cinfo.dest->next_output_byte - out_buffer;
+
+}
+
 
 } /* namespace MinVR */
