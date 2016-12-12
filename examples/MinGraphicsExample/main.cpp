a0c7de7ab6cb66efa90c1feb7f07adf8b318bf1a
Dan Orban <dtorban@umn.edu>
2016-12-12 14:17:55 -0600

    Added example usage.

--- examples/MinGraphicsExample/main.cpp
+++ examples/MinGraphicsExample/main.cpp
@@ -44,6 +44,8 @@ public:
 		// Print out when the window was opened and closed
 		if (renderState.isInitialRenderCall()) {
 			std::cout << "Window opened." << std::endl;
+
+			// Create VBOs, VAOs, Textures, Shaders, and FrameBuffers
 		}
 
 		if (!isRunning()) {
@@ -52,10 +54,16 @@ public:
 	}
 
 	void onVRRenderGraphics(VRGraphicsState& renderState) {
+		// Get projection an view matrices
+		const float* projectionMatrix = renderState.getProjectionMatrix();
+		const float* viewMatrix = renderState.getViewMatrix();
+
 		// Show gradient of red color over four seconds then restart
 		float red = std::fmod(time/4.0,1.0);
 		glClearColor(red, 0, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		// Draw calls here
 	}
 
 private:
@@ -63,6 +71,12 @@ private:
 };
 
 int main(int argc, char **argv) {
+	if (argc < 2) {
+		std::cout << "Please enter a MinVR config file..." << std::endl;
+		std::cout << "Example Usage: \n\tbuild/bin/MinGraphicsExample examples/MinGraphicsExample/desktop.xml" << std::endl;
+		return 1;
+	}
+
 	// The second argument is the config file path for this example
 	MyVRApp app(argc, argv, argv[1]);
 	app.run();
