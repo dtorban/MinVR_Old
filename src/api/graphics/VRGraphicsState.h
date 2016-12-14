79dc3d061ca6649d95e5e119b256fd65a418a5f1
Dan Orban <dtorban@umn.edu>
2016-12-13 22:13:08 -0600

    Added VRApp subclass.

--- src/api/graphics/VRGraphicsState.h
+++ src/api/graphics/VRGraphicsState.h
@@ -35,66 +35,48 @@ class VRGraphicsState {
 public:
 	VRGraphicsState(VRDataIndex& state);
 	~VRGraphicsState();
-//
-//	/** Returns a pointer to a 16 element array that stores the current projection
-//	    matrix, calculated by MinVR taking into account the current display geometry
-//	    head tracking data, and stereo rendering state.  The pointer is valid as long
-//	    as the GraphicsState object is valid.  If the MinVR DisplayGraph does not
-//	    contain a node that sets the projection matrix then the identify matrix is
-//	    returned following the same convention as OpenGL -- this is the same as
-//	    glOrtho(-1, 1, -1, 1, 1, -1).
-//	*/
+
+	/** Returns a pointer to a 16 element array that stores the current projection
+	    matrix, calculated by MinVR taking into account the current display geometry
+	    head tracking data, and stereo rendering state.  The pointer is valid as long
+	    as the GraphicsState object is valid.  If the MinVR DisplayGraph does not
+	    contain a node that sets the projection matrix then the identify matrix is
+	    returned following the same convention as OpenGL -- this is the same as
+	    glOrtho(-1, 1, -1, 1, 1, -1).
+	*/
 	const float * getProjectionMatrix() const;
-//
-//
-//	/** Returns a pointer to a 16 element array that stores the current view
-//	    matrix, calculated by MinVR taking into account the current display geometry
-//	    and head tracking data.  The pointer is valid as long as the GraphicsState
-//	    object is valid.  If the MinVR DisplayGraph does not contain a node that
-//	    sets the view matrix then the identify matrix is returned.
-//	*/
+
+
+	/** Returns a pointer to a 16 element array that stores the current view
+	    matrix, calculated by MinVR taking into account the current display geometry
+	    and head tracking data.  The pointer is valid as long as the GraphicsState
+	    object is valid.  If the MinVR DisplayGraph does not contain a node that
+	    sets the view matrix then the identify matrix is returned.
+	*/
 	const float * getViewMatrix() const;
-//
-//
-//	/** Returns a pointer to a 3 element array that stores the current camera
-//		position.  This takes stereo rendering and head tracking into account;
-//		in other words, if it is the left eye that is currently being rendered,
-//		then the (x,y,z) location of the left eye will be returned, as calculated
-//		by MinVR based on head tracking data and the current inter-ocular-distance.
-//		The same camera position can also be extracted from the view matrix - this
-//		function is provided as a convenience since users frequently need access
-//		to the current camera position.  The pointer is valid as long as the
-//		GraphicsState object exists.
-//	*/
+
+	/** Returns a pointer to a 3 element array that stores the current camera
+		position.  This takes stereo rendering and head tracking into account;
+		in other words, if it is the left eye that is currently being rendered,
+		then the (x,y,z) location of the left eye will be returned, as calculated
+		by MinVR based on head tracking data and the current inter-ocular-distance.
+		The same camera position can also be extracted from the view matrix - this
+		function is provided as a convenience since users frequently need access
+		to the current camera position.  The pointer is valid as long as the
+		GraphicsState object exists.
+	*/
 	const float* getCameraPos() const;
-//
-//
-//	const double const * getLookVec() const;
-//
-//
-//	/** Returns the near clipping plane value for the current display.  This is
-//	    typically set via a config file when defining the display geometry.  If
-//	    the MinVR DisplayGraph does not contain a node that sets the near clipping
-//	  	value, then the default value of 0.0 is returned.
-//	*/
-//	double getNearClip() const;
-//
-//
-//	/** Returns the far clipping plane value for the current display.  This is
-//	    typically set via a config file when defining the display geometry.  If
-//	    the MinVR DisplayGraph does not contain a node that sets the near clipping
-//	  	value, then the default value of 1.0 is returned.
-//	*/
-//	double getFarClip() const;
-//
-//	enum EyeType {Left, Right, Center};
-//	EyeType currentEye() const;
-//
+
+
+	/**
+	 * Returns true if the render callback is the first render call to the context, otherwise
+	 * it returns false.
+	 */
 	bool isInitialRenderCall() const;
 
 private:
 
-	// opaque ref to internal implementation state
+	// opaque reference to internal implementation state
 	class VRGraphicsStateInternal;
 	VRGraphicsStateInternal &_;
 };
